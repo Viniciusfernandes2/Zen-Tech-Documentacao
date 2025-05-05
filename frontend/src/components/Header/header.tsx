4853ba0a2b9657b62ac0891921bb0ffaac552f99
Bruno Henrique Menezes Ramos <brunoramossjc2017@gmail.com>
2025-05-05 20:35:51 -0300

    arrumando navbar

--- frontend/src/components/Header/header.tsx
+++ frontend/src/components/Header/header.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
 
 const Header: React.FC = () => {
   return (
@@ -7,14 +7,19 @@ const Header: React.FC = () => {
       <nav>
         <ul className="navList">
           <li className="home-button">
-            <a href="/home-page" className="nav-link"> Home
+            <a href="/home-page" className="nav-link">
+              {" "}
+              Home
             </a>
           </li>
-          <li className="graficos-button">
-            <a href="/grafico-page" className="nav-link"> Gráficos
+          <li className="sobre-button">
+            <a href="/Sobre-page" className="nav-link">
+              {" "}
+              Sobre
             </a>
           </li>
 
+<<<<<<< HEAD
           <li className="table-button">
             <a href="/table-page" className="nav-link"> Tabelas
             </a>
@@ -22,11 +27,26 @@ const Header: React.FC = () => {
 
           <li className="sobre-button">
             <a href="/Sobre-page" className="nav-link"> Sobre
+=======
+          <li className="graficos-button">
+            <a href="/grafico-page" className="nav-link">
+              {" "}
+              Gráficos
+            </a>
+          </li>
+
+          <li className="table-button">
+            <a href="/table-page" className="nav-link">
+              {" "}
+              table
+>>>>>>> d45d7d67bf4c9bc92863c556329ef8bc106c0f62
             </a>
           </li>
 
           <li className="sair-button">
-            <a href="/" className="nav-link"> Sair
+            <a href="/" className="nav-link">
+              {" "}
+              Sair
             </a>
           </li>
         </ul>
