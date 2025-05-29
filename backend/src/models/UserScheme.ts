235dc1e24dacc56b58f100fac80ec2fd0f48c35c
Tiago <tiagosantinidasilva@gmail.com>
2025-05-28 21:41:56 -0300

    role

--- backend/src/models/UserScheme.ts
+++ backend/src/models/UserScheme.ts
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, match: [/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "E-mail inválido!"] },
     password: { type: String, required: true, min: 5, max: 20 },
+    role: { type: String, required: true, enum: ['user', 'visitor'], default: 'visitor' },
 });
 
 const User = mongoose.model("User", UserSchema, 'users');
